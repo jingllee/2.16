28cb501db8afcb69ce38f26379086a33149f2808
jingllee <1120995155@qq.com>
2017-02-15 21:26:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/a215/Lj/Ellen.java
@@ -0,0 +1,12 @@
+package com.example.administrator.a215.Lj;
+
+/**
+ * Created by Administrator on 2017/2/15.
+ */
+
+public class Ellen {
+    public String getValue(){
+        return"开学第一天";
+
+    }
+}
